d309c726d3952c1723ba0f294af1f23bb21ff9d0
Jaslene <yunchien0102@gmail.com>
2024-12-23 16:55:28 +0800

    legend_settings

---
+++ legend_settings.py
@@ -0,0 +1,15 @@
+# legend_setting
+
+legend_labels = ["pretrained", "trained from scratch"]
+
+legend_loc = "center right"
+bbox_to_anchor=(0.85, 0.5)
+legend_fontsize = 12
+
+legend_kwarg = dict(labelspacing=1.0,
+                    handlelength=1.2,
+                    handleheight=1.2,
+                    handletextpad=1.5,
+                    borderpad=1.0,
+                    borderaxespad=1.0)
+
